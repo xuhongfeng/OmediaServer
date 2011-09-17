c8d89ec5db61647cb980470b4e6372ecd0621d3d
xuhongfeng <life.xhf@gmail.com>
2011-09-17 17:18:29 +0800

    ADD : 加上log4j

--- src/main/java/org/tsinghua/omedia/dao/AccountDAO.java
+++ src/main/java/org/tsinghua/omedia/dao/AccountDAO.java
@@ -1,5 +1,13 @@
 package org.tsinghua.omedia.dao;
 
-public interface AccountDAO extends DAO {
+import org.tsinghua.omedia.model.Account;
 
+public interface AccountDAO extends DAO {
+    public static final String TABLE_NAME = "account";
+    public static final String COL_ID = "id";
+    public static final String COL_USER_NAME = "username";
+    public static final String COL_PASSWORD = "password";
+    public static final String COL_EMAIL = "email";
+    
+    public Account getAccountByUsername(String username);
 }
--- src/main/java/org/tsinghua/omedia/dao/AccountDAOImple.java
+++ src/main/java/org/tsinghua/omedia/dao/AccountDAOImple.java
@@ -1,5 +1,11 @@
 package org.tsinghua.omedia.dao;
 
+import org.tsinghua.omedia.model.Account;
+
 public class AccountDAOImple implements AccountDAO {
+
+    public Account getAccountByUsername(String username) {
+        return null;
+    }
 
 }
